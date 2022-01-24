2574df7ec68146ec5cea030a0f6d28943fada3ed
Ziyad Edher <ziyad.edher@gmail.com>
2022-01-23 21:31:04 -0800

    Add root Next.js _document

---
+++ packages/604dao-static-site/src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+import { getStorageURI } from "../utils/storage";
+
+// eslint-disable-next-line @typescript-eslint/no-shadow -- Next.js pattern.
+const Document: React.FunctionComponent = () => (
+  <Html lang="en">
+    <Head>
+      <meta charSet="utf-8" />
+      <link rel="icon" type="image/png" href={getStorageURI("brain.png")} />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
