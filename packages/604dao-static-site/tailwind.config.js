5579cff6b28e9b04b501ad984a55543943dbc903
Ziyad Edher <ziyad.edher@gmail.com>
2022-01-23 20:57:58 -0800

    Install @tailwindcss/typography

--- packages/604dao-static-site/tailwind.config.js
+++ packages/604dao-static-site/tailwind.config.js
@@ -10,5 +10,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // eslint-disable-next-line node/global-require, @typescript-eslint/no-require-imports -- exception for configuration files.
+    require("@tailwindcss/typography"),
+  ],
 };
